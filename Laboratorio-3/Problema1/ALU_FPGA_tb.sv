145db345bc31e855c82e4ea28d77cb42a3d4a719
Saymon Astúa Madrigal <42762900+astuaTEC@users.noreply.github.com>
2021-09-09 08:58:45 -0600

    [NUEVO] Testbench
    Se realiza el testbench completo a toda la ALU

--- Laboratorio-3/Problema1/ALU_FPGA_tb.sv
+++ Laboratorio-3/Problema1/ALU_FPGA_tb.sv
@@ -12,26 +12,121 @@ module ALU_FPGA_tb();
 	ALU_FPGA myAlu(a, b, op, hex1, hex2, N, Z, C, V);
 	
 	initial begin
+		//pruebas para la suma
 		a = 15;
 		b = 15;
 		op = 0;
-		#40
+		#20
 		
 		a = 0;
 		b = 0;
-		#40
-		
-		a = 8;
-		b = 15;
-		op = 1;
-		#40
+		#20
 		
 		a = 5;
+		b = 3;
+		#20
+		
+		a = 10;
+		b = 1;
+		#20
+		
+		// pruebas para la resta
+		a = 8;
+		b = 15;
+		#20
+		
+		a = 10;
+		b = 4;
+		#20
+		
+		a = 8;
+		b = 8;
+		#20
+		
+		a = 0;
+		b = 15;
+		#20
+		
+		//pruebas para el corrimiento a la derecha
+		a = 5;
 		op = 2;
-		#40
+		#20
+		
+		a = 10;
+		#20
 		
 		a = 3;
+		#20
+		
+		a = 15;
+		#20
+		
+		//pruebas para el corrimiento a la izquierda
+		a = 3;
 		op = 3;
+		#20
+		
+		a = 4;
+		#20
+		
+		a = 12;
+		#20
+		
+		a = 5;
+		#20
+		
+		//pruebas para el and
+		a = 0;
+		b = 15;
+		op = 4;
+		#20
+		
+		a = 3;
+		b = 1;
+		#20
+		
+		a = 8;
+		b = 12;
+		#20
+		
+		a = 4;
+		b = 2;
+		#20
+		
+		//pruebas para el or
+		a = 0;
+		b = 15;
+		op = 5;
+		#20
+		
+		a = 5;
+		b = 2;
+		#20
+		
+		a = 10;
+		b = 3;
+		#20
+		
+		a = 4;
+		b = 2;
+		#20
+		
+		//pruebas para el xor
+		a = 7;
+		b = 3;
+		op = 6;
+		#20
+		
+		a = 8;
+		b = 1;
+		#20
+		
+		a = 14;
+		b = 5;
+		#20
+		
+		a = 3;
+		b = 2;
 	end
 
 endmodule 
